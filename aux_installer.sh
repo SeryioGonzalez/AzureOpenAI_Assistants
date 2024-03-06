39b0c648a53751a443bcb50a6d7d7ef97c141649
sergiogon <sergiogon@microsoft.com>
2024-03-06 19:28:29 +0100

    aux

--- aux_installer.sh
+++ aux_installer.sh
@@ -1,6 +1,11 @@
+#/bin/bash
 sudo apt-get update -y
 sudo apt-get install python3 -y 
 sudo apt-get install python3-pip -y 
 sudo apt-get install python-is-python3 -y 
 
-pip install -r aux_requirements.txt
+pip install streamlit==1.30.0
+pip install python-dotenv==1.0.0
+pip install openai==1.12.0
+
+git clone https://github.com/SeryioGonzalez/AzureOpenAI_Assistants.git /home/streamlit/AzureOpenAI_Assistants
